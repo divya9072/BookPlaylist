4219200ddc246013a19651aea213ad8292cfd3d8
divya9072 <divi9072@gmail.com>
2022-02-01 19:36:58 +0530

    first commit

---
+++ server/app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+const schema = require('./schema/schema');
+const mongoose = require("mongoose");
+
+const app = express();
+
+const URL ="mongodb+srv://Divya9072:Divimg9072@cluster0.dvcx3.mongodb.net/usertable_details?retryWrites=true&w=majority"
+mongoose.connect(
+  URL,
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true, 
+  },
+  () => console.log("DB CONNECTED")
+);
+
+// bind express with graphql
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql:true
+}));
+
+app.listen(4000, () => {
+    console.log('now listening for requests on port 4000');
+});
