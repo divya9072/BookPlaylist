01a8c9c44e8dc5b38415746410a3b0926fcd5be9
divya9072 <divi9072@gmail.com>
2022-02-02 19:00:53 +0530

    second commit

--- server/app.js
+++ server/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const {graphqlHTTP} = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require("mongoose");
+const cors=require('cors')
 
 const app = express();
+
+app.use(cors())
 
 const URL ="mongodb+srv://Divya9072:Divimg9072@cluster0.dvcx3.mongodb.net/usertable_details?retryWrites=true&w=majority"
 mongoose.connect(
